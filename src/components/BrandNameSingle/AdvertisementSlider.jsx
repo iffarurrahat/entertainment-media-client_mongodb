97caf8ba56d1567738be385662fba72027633a38
Iffarur <iffarurrahat@gmail.com>
2023-10-25 23:23:52 +0600

    adde exta two secion in my projects

--- src/components/BrandNameSingle/AdvertisementSlider.jsx
+++ src/components/BrandNameSingle/AdvertisementSlider.jsx
@@ -2,7 +2,29 @@
 const AdvertisementSlider = () => {
     return (
         <div>
-            <h2>Lorem ipsum dolor sit amet.</h2>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img  src="https://i.ibb.co/6ydNzr6/addver-2.jpg" className="w-full h-80" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2 left-5 right-5">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/6ydNzr6/addver-2.jpg" className="w-full h-80" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2 left-5 right-5">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/6ydNzr6/addver-2.jpg" className="w-full h-80" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2 left-5 right-5">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
