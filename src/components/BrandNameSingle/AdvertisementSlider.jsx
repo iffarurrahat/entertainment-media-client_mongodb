90768f3289d571b683628c1c551dd19b4be1a407
Iffarur <iffarurrahat@gmail.com>
2023-10-25 23:06:51 +0600

    AdvertisementSlider section added on my projects

---
+++ src/components/BrandNameSingle/AdvertisementSlider.jsx
@@ -0,0 +1,10 @@
+
+const AdvertisementSlider = () => {
+    return (
+        <div>
+            <h2>Lorem ipsum dolor sit amet.</h2>
+        </div>
+    );
+};
+
+export default AdvertisementSlider;
--- src/components/BrandNameSingle/BrandNameSingle.jsx
+++ src/components/BrandNameSingle/BrandNameSingle.jsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData, useParams, } from "react-router-dom";
 import { AiOutlineDoubleRight } from 'react-icons/ai';
 import BrandNameSingleCard from "./BrandNameSingleCard";
 import { useState } from "react";
+import AdvertisementSlider from "./AdvertisementSlider";
 
 const BrandNameSingle = () => {
 
@@ -9,7 +10,7 @@ const BrandNameSingle = () => {
 
     // database data
     const loadedProducts = useLoaderData();
-    
+
     const filter = loadedProducts.filter(data => data.brandName == params.id);
     const [products, setProducts] = useState(filter)
 
@@ -22,15 +23,24 @@ const BrandNameSingle = () => {
                     <li className="text-[#d926a9]">Brand Product</li>
                 </ul>
             </div>
-            <div className="max-w-screen-xl mx-auto py-10">
-                <div className="grid md:grid-cols-2 gap-6 px-4 md:px-3 lg:px-2">
-                    {
-                        products.map(product => <BrandNameSingleCard
-                            key={product._id}
-                            product={product}
-                            products={products}
-                            setProducts={setProducts}></BrandNameSingleCard>)
-                    }
+
+
+            <div>
+                {/* Advertisement Slider */}
+                <div className='py-5 mb-8'>
+                    <AdvertisementSlider></AdvertisementSlider>
+                </div>
+                {/* Brand Card */}
+                <div className="max-w-screen-xl mx-auto py-10">
+                    <div className="grid md:grid-cols-2 gap-6 px-4 md:px-3 lg:px-2">
+                        {
+                            products.map(product => <BrandNameSingleCard
+                                key={product._id}
+                                product={product}
+                                products={products}
+                                setProducts={setProducts}></BrandNameSingleCard>)
+                        }
+                    </div>
                 </div>
             </div>
         </div>
