9b1369e13da0c191d473653515be5da63870fbc4
Iffarur <iffarurrahat@gmail.com>
2023-10-25 23:29:12 +0600

    added login page with email/password and gmail or github

--- src/components/BrandNameSingle/AdvertisementSlider.jsx
+++ src/components/BrandNameSingle/AdvertisementSlider.jsx
@@ -4,7 +4,7 @@ const AdvertisementSlider = () => {
         <div>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img  src="https://i.ibb.co/6ydNzr6/addver-2.jpg" className="w-full h-80" />
+                    <img  src="https://i.ibb.co/VxRCSGM/addver-1.jpg" className="w-full h-80" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 top-1/2 left-5 right-5">
                         <a href="#slide4" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
